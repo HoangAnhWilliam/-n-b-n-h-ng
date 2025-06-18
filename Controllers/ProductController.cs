a8557e82ec3977949ca26aeb1ea1d3ee240870e0
HoangAnhWilliam <hoanganhgianguyen@hotmai.com>
2025-06-18 23:02:59 +0700

    Initial commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using HotelBooking.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBooking.Controllers
+{
+    public class ProductController : Controller
+    {   
+        private readonly DataContext _dataContext;
+
+        public ProductController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		public async Task<IActionResult> Details(int Id)
+		{
+			if (Id == null) return RedirectToAction("Index");
+
+			var productsById = _dataContext.Products
+				.Where(p => p.Id == Id)
+				.FirstOrDefault();
+
+            var relatedProducts = await _dataContext.Products.Where(p => p.CategoryId == productsById.CategoryId && p.Id != productsById.Id).Take(4).ToListAsync();
+            ViewBag.RelatedProducts = relatedProducts;
+			return View(productsById);
+		}
+
+        public async Task<IActionResult> Search(string searchTerm)
+        {
+            var products = await _dataContext.Products.Where(p=>p.Name.Contains(searchTerm) || p.Description.Contains(searchTerm)).ToListAsync();
+            ViewBag.Keyword = searchTerm;
+            return View(products);
+        }
+	}
+}
